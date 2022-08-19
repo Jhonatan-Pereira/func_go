4df8d9361d578be01c1e7c815cc6f088d1e6cc26
Jhonatan Pereira <jhonatanfrade.pereira@gmail.com>
2022-08-19 00:00:56 -0300

    feat: str util

---
+++ utils/str_util.go
@@ -0,0 +1,20 @@
+package str_util
+
+import "unicode"
+
+func ToUpper(s string) string {
+	r := []rune(s)
+	for i := range r {
+		r[i] = unicode.ToUpper(r[i])
+	}
+	return string(r)
+}
+
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(s)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[j]
+	}
+
+	return string(r)
+}
