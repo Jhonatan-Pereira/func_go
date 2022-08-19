59290951d25fcfd0e880fd22a228744a5c174ed8
Jhonatan Pereira <jhonatanfrade.pereira@gmail.com>
2022-08-19 00:28:54 -0300

    fix: bugs

--- strutils/strutils.go
+++ strutils/strutils.go
@@ -13,7 +13,7 @@ func ToUpper(s string) string {
 func Reverse(s string) string {
 	r := []rune(s)
 	for i, j := 0, len(s)-1; i < len(r)/2; i, j = i+1, j-1 {
-		r[i], r[j] = r[j], r[j]
+		r[i], r[j] = r[j], r[i]
 	}
 
 	return string(r)
